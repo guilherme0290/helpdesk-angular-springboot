277f7c8e27ee5ad20346135c959e3d9a58cae36f
guilherme.ferreira <guilherme.ferreira@foton.la>
2022-10-26 15:18:27 -0400

    implementado metodo de logout

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
-  {
-    path:'login',component: LoginComponent
-  },
+  { path:'login',component: LoginComponent },
   {   
-    path:'',component: NavComponent,children:[
-      {
-        path:'home',component: HomeComponent
-      },
-      {
-        path:'tecnicos',component: TecnicoListComponent
-      }
+    path:'',component: NavComponent,canActivate:[AuthGuard], children:[
+      {path:'home',component: HomeComponent},
+      {path:'tecnicos',component: TecnicoListComponent}
     ]
   }
   
--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, MinLengthValidator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -22,18 +24,21 @@ export class LoginComponent implements OnInit {
     
   }
   
-  constructor(private toast: ToastrService) { }
+  constructor(private toast: ToastrService,
+              private service: AuthService,
+              private router: Router) { }
 
   validaCampos(): boolean{
-    if(this.email.valid && this.senha.valid){
-      return true;
-    }
-    return false;    
+    return this.email.valid && this.senha.valid
   }
 
   logar(){
-    this.toast.error('Usuário e/ou senha inválidas','Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta =>{      
+      this.service.successfullLogin(resposta.headers.get('Authorization').substring(7))
+      this.router.navigate(['home'])
+   },()=>{
+      this.toast.error('Usuário e/ou senha invalida')
+   })
   }
 
 }
