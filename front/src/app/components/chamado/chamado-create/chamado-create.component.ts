48c0b3cfa180fc98a68347f63bde9e3243ff2f05
guilherme.ferreira <guilherme.ferreira@foton.la>
2022-11-01 20:26:35 -0400

    servico para criar chamados implementado

--- front/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ front/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/service/chamado.service';
+import { ClienteService } from 'src/app/service/cliente.service';
+import { TecnicoService } from 'src/app/service/tecnico.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -8,16 +16,50 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ChamadoCreateComponent implements OnInit {
 
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
+
+  chamado: Chamado = {
+    prioridade:     '',
+    status:         '',
+    titulo:         '',
+    observacoes:      '',
+    tecnico:        '',
+    cliente:        '',
+    nomeCliente:    '',
+    nomeTecnico:    ''
+  }
+
   prioridade: FormControl = new FormControl(null,[Validators.required])
   status:     FormControl = new FormControl(null,[Validators.required])
   titulo:     FormControl = new FormControl(null,[Validators.required])
-  descricao:  FormControl = new FormControl(null,[Validators.required])
+  observacoes:  FormControl = new FormControl(null,[Validators.required])
   tecnico:    FormControl = new FormControl(null,[Validators.required])
   cliente:    FormControl = new FormControl(null,[Validators.required])
 
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private chamadoService: ChamadoService,
+    private toast:          ToastrService,
+    private router:         Router
+  ) { }
 
   ngOnInit(): void {
+    this.findAllClientes()
+    this.findAllTecnicos()
+  }
+
+  findAllClientes(): void{
+    this.clienteService.findAll().subscribe(resposta =>{
+      this.clientes = resposta;
+    })
+  }
+
+  findAllTecnicos(): void{
+    this.tecnicoService.findAll().subscribe(resposta =>{
+      this.tecnicos= resposta;
+    })
   }
 
   validaCampos(): boolean{
@@ -26,7 +68,17 @@ export class ChamadoCreateComponent implements OnInit {
     this.titulo.valid &&
     this.tecnico.valid &&
     this.cliente.valid &&
-    this.descricao.valid
+    this.observacoes.valid
+  }
+
+  create():void{
+    this.chamadoService.create(this.chamado).subscribe(resposta => {
+      this.toast.success('Chamado criado com sucesso','Novo Chamado')
+        this.router.navigate(['chamados'])
+    },ex =>{
+      console.log(ex)
+      this.toast.error(ex.error.error)
+    })
   }
 
 }
